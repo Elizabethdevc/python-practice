1938a178ac75211e7d0992e1802f090d85cec955
Elizabethdevc <melvsween@gmail.com>
2026-05-15 14:41:50 -0700

    Add calculator script

---
+++ calculator.py
@@ -0,0 +1,11 @@
+Python 3.13.13 (tags/v3.13.13:01104ce, Apr  7 2026, 19:25:48) [MSC v.1944 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> num1 = float(input("First number: "))
+... num2 = float(input("Second number: "))
+...
+... print("Addition:", num1 + num2)
+... print("Subtraction:", num1 - num2)
+... print("Multiplication:", num1 * num2)
+... print("Division:", num1 / num2)
+...
+First number:
